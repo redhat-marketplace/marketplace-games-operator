eb1b7d4b448522bd2cec3175dafdea6bdc084400
Richard Gebhardt <gebhardt@us.ibm.com>
2020-10-30 12:18:26 -0400

    cleanup boilerplate

--- api/v1alpha1/arcade_types.go
+++ api/v1alpha1/arcade_types.go
@@ -20,22 +20,14 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // ArcadeSpec defines the desired state of Arcade
 type ArcadeSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of Arcade. Edit Arcade_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
 }
 
 // ArcadeStatus defines the observed state of Arcade
 type ArcadeStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
 // +kubebuilder:object:root=true
