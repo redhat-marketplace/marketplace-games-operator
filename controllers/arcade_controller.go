0e3a1bdb834f616b5367f35556c6db064ed72679
Adams Ombonga <adamsombonga@gmail.com>
2020-11-20 10:59:39 -0500

    feat: add Size to operator Spec and Status to API in prep for certification

--- controllers/arcade_controller.go
+++ controllers/arcade_controller.go
@@ -18,6 +18,9 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"time"
 
 	"github.com/go-logr/logr"
 	routev1 "github.com/openshift/api/route/v1"
@@ -28,11 +31,15 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	intstr "k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/wait"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	arcadev1alpha1 "github.com/redhat-marketplace/marketplace-games-operator/api/v1alpha1"
 )
+
+// PORT which container application will be exposed
+const PORT = 8080
 
 // ArcadeReconciler reconciles a Arcade object
 type ArcadeReconciler struct {
@@ -64,14 +71,22 @@ func (r *ArcadeReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		return ctrl.Result{}, err
 	}
 
+	nameSpaceKey := types.NamespacedName{Name: arcade.Name, Namespace: arcade.Namespace}
+	arcade.Status.ArcadeStatus = arcadev1alpha1.ArcadeStatusPending
+
 	deployment := &appsv1.Deployment{}
-	err = r.Get(ctx, types.NamespacedName{Name: arcade.Name, Namespace: arcade.Namespace}, deployment)
+	err = r.Get(ctx, nameSpaceKey, deployment)
 	if err != nil && errors.IsNotFound(err) {
 		dep := r.deploymentForArcade(arcade)
 		log.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
 		err = r.Create(ctx, dep)
 		if err != nil {
 			log.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			reason := fmt.Sprintf("Failed to create Deployment %s", err)
+			arcade.Status.ArcadeStatus = arcadev1alpha1.ArcadeStatusFailure
+			arcade.Status.Reason = reason
+			// Update Status
+			r.updateStatus(arcade, nameSpaceKey)
 			return ctrl.Result{}, err
 		}
 		return ctrl.Result{Requeue: true}, nil
@@ -81,13 +96,17 @@ func (r *ArcadeReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	}
 
 	service := &corev1.Service{}
-	err = r.Get(ctx, types.NamespacedName{Name: arcade.Name, Namespace: arcade.Namespace}, service)
+	err = r.Get(ctx, nameSpaceKey, service)
 	if err != nil && errors.IsNotFound(err) {
 		svc := r.serviceForArcade(arcade)
 		log.Info("Creating a new Service", "Service.Namespace", svc.Namespace, "Service.Name", svc.Name)
 		err = r.Create(ctx, svc)
 		if err != nil {
 			log.Error(err, "Failed to create new Service", "Service.Namespace", svc.Namespace, "Service.Name", svc.Name)
+			reason := fmt.Sprintf("Failed to create Service %s", err)
+			arcade.Status.ArcadeStatus = arcadev1alpha1.ArcadeStatusFailure
+			arcade.Status.Reason = reason
+			r.updateStatus(arcade, nameSpaceKey)
 			return ctrl.Result{}, err
 		}
 		return ctrl.Result{Requeue: true}, nil
@@ -97,20 +116,42 @@ func (r *ArcadeReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	}
 
 	route := &routev1.Route{}
-	err = r.Get(ctx, types.NamespacedName{Name: arcade.Name, Namespace: arcade.Namespace}, route)
+	err = r.Get(ctx, nameSpaceKey, route)
 	if err != nil && errors.IsNotFound(err) {
 		rt := r.routeForArcade(arcade)
 		log.Info("Creating a new Route", "Route.Namespace", rt.Namespace, "Route.Name", rt.Name)
 		err = r.Create(ctx, rt)
 		if err != nil {
 			log.Error(err, "Failed to create new Route", "Route.Namespace", rt.Namespace, "Route.Name", rt.Name)
+			reason := fmt.Sprintf("Failed to create Route %s", err)
+			arcade.Status.ArcadeStatus = arcadev1alpha1.ArcadeStatusFailure
+			arcade.Status.Reason = reason
+			r.updateStatus(arcade, nameSpaceKey)
 			return ctrl.Result{}, err
 		}
-		return ctrl.Result{Requeue: true}, nil
+		return ctrl.Result{RequeueAfter: time.Second * 15}, nil
 	} else if err != nil {
-		log.Error(err, "Failed to get Service")
+		log.Error(err, "Failed to get Route")
 		return ctrl.Result{}, err
 	}
+
+	// Ensure Size matches our given Spec
+	size := arcade.Spec.Size
+	if *deployment.Spec.Replicas != size {
+		deployment.Spec.Replicas = &size
+		err = r.Update(ctx, deployment)
+		if err != nil {
+			log.Error(err, "Failed to update Deployment", "Deployment.Namespace", deployment.Namespace, "Deployment.Name", deployment.Name)
+			return ctrl.Result{}, err
+		}
+		// Return and requeue once Spec is updated
+		return ctrl.Result{Requeue: true}, nil
+	}
+
+	// Set OK Status
+	arcade.Status.ArcadeStatus = arcadev1alpha1.ArcadeStatusOK
+	arcade.Status.Reason = ""
+	r.updateStatus(arcade, nameSpaceKey)
 
 	return ctrl.Result{}, nil
 }
@@ -123,6 +164,36 @@ func (r *ArcadeReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Owns(&corev1.Service{}).
 		Owns(&routev1.Route{}).
 		Complete(r)
+}
+
+func (r *ArcadeReconciler) updateStatus(arcade *arcadev1alpha1.Arcade, nameSpaceKey types.NamespacedName) {
+	pollInteraval := time.Millisecond * 500
+	timeout := time.Second * 30
+	ctx := context.TODO()
+
+	r.Log.Info("Updating status for ", "Arcade instance", arcade.Name)
+	err := wait.Poll(pollInteraval, timeout, func() (bool, error) {
+		if updateErr := r.Status().Update(ctx, arcade); errors.IsConflict(updateErr) {
+			updatedArcade := &arcadev1alpha1.Arcade{ObjectMeta: metav1.ObjectMeta{
+				Name:      arcade.Name,
+				Namespace: arcade.Namespace,
+			}}
+			if err := r.Get(context.TODO(), nameSpaceKey, updatedArcade); err != nil {
+				return false, err
+			}
+			// we override only the spec, which we are interested into
+			updatedArcade.Status = arcade.Status
+			arcade = updatedArcade
+			return false, nil
+		} else if updateErr != nil {
+			return false, updateErr
+		}
+		return true, nil
+	})
+	if err != nil {
+		r.Log.Error(err, "Error while updating status")
+	}
+
 }
 
 // deploymentForArcade returns a Arcade Deployment object
@@ -144,18 +215,21 @@ func (r *ArcadeReconciler) deploymentForArcade(m *arcadev1alpha1.Arcade) *appsv1
 				},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{{
-						Image: "arcade:1.0.0",
+						Image: os.Getenv("REGISTRY_REPO") + "." + os.Getenv("REGISTRY_HOST") + "/rhm-arcade",
 						Name:  "arcade",
 						Ports: []corev1.ContainerPort{{
-							ContainerPort: 4004,
+							ContainerPort: PORT,
 							Name:          "arcade",
 						}},
+					}},
+					ImagePullSecrets: []corev1.LocalObjectReference{{
+						Name: "regcreds-arcade-art",
 					}},
 				},
 			},
 		},
 	}
-	// Set Arcade instance as the owner and controller
+	// Set Arcade instance as the owner and controller for deployments
 	ctrl.SetControllerReference(m, dep, r.Scheme)
 	return dep
 }
@@ -172,14 +246,16 @@ func (r *ArcadeReconciler) serviceForArcade(m *arcadev1alpha1.Arcade) *corev1.Se
 		Spec: corev1.ServiceSpec{
 			Type: "ClusterIP",
 			Ports: []corev1.ServicePort{{
-				Port:       4004,
-				Protocol:   "TCP",
-				TargetPort: intstr.FromInt(4004),
+				Port:       PORT,
+				Protocol:   corev1.ProtocolTCP,
+				TargetPort: intstr.FromInt(PORT),
 			}},
 			Selector: ls,
 		},
 	}
 
+	// Set Arcade instance as the owner and controller for created services
+	ctrl.SetControllerReference(m, service, r.Scheme)
 	return service
 }
 
@@ -187,6 +263,9 @@ func (r *ArcadeReconciler) serviceForArcade(m *arcadev1alpha1.Arcade) *corev1.Se
 func (r *ArcadeReconciler) routeForArcade(m *arcadev1alpha1.Arcade) *routev1.Route {
 	ls := labelsForArcade(m.Name)
 	route := &routev1.Route{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "Route",
+		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      m.Name,
 			Namespace: m.Namespace,
@@ -198,7 +277,7 @@ func (r *ArcadeReconciler) routeForArcade(m *arcadev1alpha1.Arcade) *routev1.Rou
 				Name: m.Name,
 			},
 			Port: &routev1.RoutePort{
-				TargetPort: intstr.FromString(m.Name),
+				TargetPort: intstr.FromInt(PORT),
 			},
 			TLS: &routev1.TLSConfig{
 				Termination: routev1.TLSTerminationEdge,
@@ -206,7 +285,7 @@ func (r *ArcadeReconciler) routeForArcade(m *arcadev1alpha1.Arcade) *routev1.Rou
 		},
 	}
 
-	// Set MobileSecurityService mss as the owner and controller
+	// Set Arcade instance as the owner and controller for created routes
 	ctrl.SetControllerReference(m, route, r.Scheme)
 	return route
 }
